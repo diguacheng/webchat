d38639dd4219250a8a336e2fb9429c0829674578
digua <42920772+diguacheng@users.noreply.github.com>
2020-06-29 21:28:03 +0800

    Add files via upload

---
+++ data.go
@@ -0,0 +1,10 @@
+package main
+
+type Data struct {
+	IP     string `json:"ip"`
+	Type    string `json:"type"`
+	User    string `json:"user"`
+	Form    string `json:"from"`
+	FormIP string `json:"FromIp"`
+	Content string `json:"content"`
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+这是一个实时聊天室的小demo
+
+主要目的是学习使用  "github.com/gorilla/websocket"包构建websocket服务的
+
+usage：
+
+`go run main.go hub.go data.go client.go`
+
+
+
+参考：
+
+`https://github.com/gorilla/websocket/examples/chat`
